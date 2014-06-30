543a80b3ef83646e75e8401a07adc0c35febbce7
William Candillon <wcandillon@gmail.com>
2014-06-30 14:31:01 +0200

    Bug fixes

--- tests/cli_test.js
+++ tests/cli_test.js
@@ -1,18 +1,20 @@
 'use strict';
 
 var vows = require('vows');
-//var assert = require('assert');
+var assert = require('assert');
 
 var cli = require('../lib/cli.js');
 
 vows.describe('28/cli').addBatch({
     'Test Help': {
         'login without email': function () {
-            try {
-                cli.parse(['28', 'login']);
-                throw new Error();
-            } catch(error) {
-            }
+            cli.parse(['28', 'login']);
+            assert.equal(cli.args.length, 0);
+        },
+        'login with email': function () {
+            cli.parse(['28', 'login', 'w+test@28.io']);
+            assert.equal(cli.args.length, 1);
+            assert.equal(cli.args[0], 'w+test@28.io');
         }
     }
 }).export(module);
